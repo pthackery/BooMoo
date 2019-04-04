b6322cb1b2859e8a0c43971855bede9936174759
HARRYPOTTER\Payton <pcthacke@umail.iu.edu>
2019-04-04 17:53:26 -0400

    visual updater added in:

---
+++ workers/unity/Assets/Scripts/Player/VisualUpdater.cs
@@ -0,0 +1,21 @@
+﻿using Improbable;
+using Improbable.Gdk.GameObjectRepresentation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VisualUpdater : MonoBehaviour
+{
+
+    [Require] private Position.Requirable.Reader positionReader;
+    void OnPositionUpdate(Coordinates update)
+    {
+        Vector3 newPos = new Vector3((float)update.X, (float)update.Y, (float)update.Z);
+        gameObject.transform.position = newPos;
+    }
+    private void OnEnable()
+    {
+        //Debug.LogError("Launching");
+        positionReader.CoordsUpdated += OnPositionUpdate;
+    }
+    //why is this so complicated
+}
